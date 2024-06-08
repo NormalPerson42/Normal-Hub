fc96214047ccc074c0f55fc357fa2cf7d7219b47
NormalPerson42 <shevchenko9480@gmail.com>
2024-06-08 23:56:54 +0200

    Update main.lua

--- Games/Build A Boat For Treasure/main.lua
+++ Games/Build A Boat For Treasure/main.lua
@@ -1,5 +1,5 @@
 while true do
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/main/Games/Build%20A%20Boat%20For%20Treasure/CreateBlocks.lua"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/main/Games/Build%20A%20Boat%20For%20Treasure/StartAutoFarm.lua"))()
-    wait(20)
+    wait(17)
 end
