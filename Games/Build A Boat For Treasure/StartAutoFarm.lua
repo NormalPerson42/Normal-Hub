0157b7d1932c0ea79ad58948f1b82f5e8960c639
NormalPerson42 <shevchenko9480@gmail.com>
2024-06-08 23:19:22 +0200

    Update StartAutoFarm.lua

--- Games/Build A Boat For Treasure/StartAutoFarm.lua
+++ Games/Build A Boat For Treasure/StartAutoFarm.lua
@@ -1,1 +1,40 @@
-soon
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+        -- Stage 1 --
+        local Stage1Part = workspace.AutoFarmFolder.Part1ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1Part.CFrame
+        wait(3)
+        -- Chest Stage -- 
+        local ChestPart = workspace.AutoFarmFolder.ChestPartForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = ChestPart.CFrame
+        wait(0.2)
+        -- Stage 2 --
+        local Stage2Part = workspace.AutoFarmFolder.Part2ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2Part.CFrame
+        wait(1.5)
+        -- Stage 3 --
+        local Stage3Part = workspace.AutoFarmFolder.Part3ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage3Part.CFrame
+        wait(1.5)
+        -- Stage 4 --
+        local Stage4Part = workspace.AutoFarmFolder.Part4ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage4Part.CFrame
+        wait(2)
+        -- Stage 5 --
+        local Stage5Part = workspace.AutoFarmFolder.Part5ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage5Part.CFrame
+        wait(2)
+        -- Stage 6 --
+        local Stage6Part = workspace.AutoFarmFolder.Part6ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage6Part.CFrame
+        wait(2)
+        -- Stage 7 --
+        local Stage7Part = workspace.AutoFarmFolder.Part7ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage7Part.CFrame
+        wait(1.5)
+        -- Stage 8 --
+        local Stage8Part = workspace.AutoFarmFolder.Part8ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage8Part.CFrame
+        wait(2)
+        -- Stage 9 --
+        local Stage9Part = workspace.AutoFarmFolder.Part9ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage9Part.CFrame
