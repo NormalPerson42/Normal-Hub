0e64071ec00b471e51ac0e52bd532de3406ddc0d
NormalPerson42 <shevchenko9480@gmail.com>
2024-06-09 08:40:14 +0200

    fixed errors

--- Games/Build A Boat For Treasure/main.lua
+++ Games/Build A Boat For Treasure/main.lua
@@ -179,7 +179,68 @@ function CreateParts()
 end
 
 function StartAutoFarm()
-
+    if game.Players.LocalPlayer.Character then
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1
+        -- Stage 1 --
+        local Stage1Part = workspace.AutoFarmFolder.Part1ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1Part.CFrame
+        wait(3)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Chest Stage -- 
+        local ChestPart = workspace.AutoFarmFolder.ChestPartForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = ChestPart.CFrame
+        wait(0.2)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 2 --
+        local Stage2Part = workspace.AutoFarmFolder.Part2ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2Part.CFrame
+        wait(2)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 3 --
+        local Stage3Part = workspace.AutoFarmFolder.Part3ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage3Part.CFrame
+        wait(1.5)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 4 --
+        local Stage4Part = workspace.AutoFarmFolder.Part4ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage4Part.CFrame
+        wait(2)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 5 --
+        local Stage5Part = workspace.AutoFarmFolder.Part5ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage5Part.CFrame
+        wait(2)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 6 --
+        local Stage6Part = workspace.AutoFarmFolder.Part6ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage6Part.CFrame
+        wait(1.5)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 7 --
+        local Stage7Part = workspace.AutoFarmFolder.Part7ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage7Part.CFrame
+        wait(1.5)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 8 --
+        local Stage8Part = workspace.AutoFarmFolder.Part8ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage8Part.CFrame
+        wait(2)
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        -- Stage 9 --
+        local Stage9Part = workspace.AutoFarmFolder.Part9ForAutoFarm
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage9Part.CFrame
+                                                                                end
+                                                                        end
+                                                                end
+                                                        end
+                                                end
+                                        end
+                                end
+                        end
+                end
+        end
+end
 end
 
 while true do
