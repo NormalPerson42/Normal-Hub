f4b3148ff000d0dae59dce0a41dfd9dea652cc24
NormalPerson42 <shevchenko9480@gmail.com>
2024-06-09 10:39:48 +0200

    Added Babft

--- Games/GamesHandler.lua
+++ Games/GamesHandler.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 537413528 then
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/main/Games/Build%20A%20Boat%20For%20Treasure/main.lua"))()
 end
