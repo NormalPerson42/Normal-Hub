27f5cfd17932480d667ae4098f28d2766030be7c
NormalPerson42 <shevchenko9480@gmail.com>
2024-07-22 12:54:21 +0200

    Update main.lua

--- Games/MurderMystery2/main.lua
+++ Games/MurderMystery2/main.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/main/Games/MurderMystery2/ModifiedUI.lua'))()
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    LoadingTitle = "Rayfield Interface Suite",
