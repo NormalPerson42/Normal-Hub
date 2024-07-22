2b2c789e39ac68881059381e24c057cbb2cb30e3
NormalPerson42 <shevchenko9480@gmail.com>
2024-07-22 13:24:09 +0200

    Update main.lua

--- Games/MurderMystery2/main.lua
+++ Games/MurderMystery2/main.lua
@@ -1,2 +1,45 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/main/Games/MurderMystery2/ModifiedUI.lua')))()
+local Window = OrionLib:MakeWindow({Name = "Normal H", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Key = ""
+
+-- Functions --
+function CheckKey()
+
+end
+
+function GetKey()
+
+end
+
+local Main = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Main:AddTextbox({
+	Name = "Enter Key: ",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		 Key = Value
+	end	  
+})
+
+Main:AddButton({
+	Name = "Check Key",
+	Callback = function()
+      		CheckKey()
+  	end    
+})
+
+Main:AddButton({
+	Name = "Get Key",
+	Callback = function()
+      		CheckKey()
+  	end    
+})
+
+Main:AddLabel("Your HWID: "..game:GetService("RbxAnalyticsService"):GetClientId())
+
+OrionLib:Init()
