9cd74a14acb0d6ae8f30e3c9394e421d8d338a62
NormalPerson42 <shevchenko9480@gmail.com>
2024-09-28 13:54:58 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -82,6 +82,14 @@ TimeLeft.BorderSizePixel = 0
 TimeLeft.Position = UDim2.new(0, 0, 1, 0)
 TimeLeft.Size = UDim2.new(1, 0, -0.0700000003, 0)
 
+function CheckGame()
+	if game.PlaceId == 6839171747 then -- doors game
+		if game.ReplicatedStorage.GameData.SecretFloor == true then
+			return "Doors_TheBackdoor"
+		end
+	end
+end
+
 local vers = "0.3.2"
 local RunningLatestVersion = false
 local RunningKeySystem = false
@@ -494,6 +502,45 @@ if not Exitted then
 		end
 	end
 
+	function notify(title, desc, duration)
+		local sound = Instance.new("Sound", game.ReplicatedStorage)
+		sound.SoundId = "rbxassetid://4590657391"
+		sound.Volume = 2
+		sound:Play()
+
+		local notificationframe = placeholderframe_2:Clone()
+		notificationframe.Parent = placeholderframe_2.Parent
+		notificationframe.Title.Text = title
+		notificationframe.Desc.Text = desc
+
+		notificationframe:TweenSize(
+			UDim2.new(0.265, 0, 0.083, 0),
+			Enum.EasingDirection.In,
+			Enum.EasingStyle.Linear,
+			0.25,
+			false
+		)
+		wait(0.25)
+		sound:Destroy()
+		notificationframe.TimeLeft:TweenSize(
+			UDim2.new(0, 0, -0.0700000003, 0),
+			Enum.EasingDirection.In,
+			Enum.EasingStyle.Linear,
+			duration,
+			false
+		)
+		wait(duration)
+		notificationframe:TweenSize(
+			UDim2.new(0, 0, 0.083, 0),
+			Enum.EasingDirection.In,
+			Enum.EasingStyle.Linear,
+			0.25,
+			false
+		)
+		wait(0.25)
+		notificationframe:Destroy()
+	end
+
 	if CheckAuth() == "NoKey" and not Exitted then
 		RunningKeySystem = true
 		Status.Text = "Key not found! Launching the key system..."
@@ -638,45 +685,6 @@ if not Exitted then
 			exit.TextSize = 14.000
 			exit.TextWrapped = true
 
-			function notify(title, desc, duration)
-				local sound = Instance.new("Sound", game.ReplicatedStorage)
-				sound.SoundId = "rbxassetid://4590657391"
-				sound.Volume = 2
-				sound:Play()
-
-				local notificationframe = placeholderframe_2:Clone()
-				notificationframe.Parent = placeholderframe_2.Parent
-				notificationframe.Title.Text = title
-				notificationframe.Desc.Text = desc
-
-				notificationframe:TweenSize(
-					UDim2.new(0.265, 0, 0.083, 0),
-					Enum.EasingDirection.In,
-					Enum.EasingStyle.Linear,
-					0.25,
-					false
-				)
-				wait(0.25)
-				sound:Destroy()
-				notificationframe.TimeLeft:TweenSize(
-					UDim2.new(0, 0, -0.0700000003, 0),
-					Enum.EasingDirection.In,
-					Enum.EasingStyle.Linear,
-					duration,
-					false
-				)
-				wait(duration)
-				notificationframe:TweenSize(
-					UDim2.new(0, 0, 0.083, 0),
-					Enum.EasingDirection.In,
-					Enum.EasingStyle.Linear,
-					0.25,
-					false
-				)
-				wait(0.25)
-				notificationframe:Destroy()
-			end
-
 			checkkey.MouseButton1Click:Connect(function()
 				if keyinput.Text == "trial" then
 					if not isfolder("Normal Hub") then
@@ -783,16 +791,31 @@ if not Exitted then
 		end
 		wait(math.random(9, 11) / 10)
 		if not Exitted then
-			LoadingScreen:Destroy()
+			-- testing purposes --
 			local key = readfile("Normal Hub//key.txt")
 			if key == "trial" then
-				notify("Test", "Freemium", 3)
+				spawn(function()
+					notify("Test", "Freemium", 3)
+				end)
 			elseif key == "trial2" then
-				notify("Test", "Premium", 3)
+				spawn(function()
+					notify("Test", "Premium", 3)
+				end)
 			elseif key == "NoKeyTillNextLaunch" then
-				notify("Test", "Lite (no key)", 3)
+				spawn(function()
+					notify("Test", "Lite (no key)", 3)
+				end)
 			else
-				notify("Test", "No key at all", 3)
+				spawn(function()
+					notify("Test", "No key at all", 3)
+				end)
+			end
+			LoadingScreen:Destroy()
+			if CheckGame() == "Doors_TheBackdoor" then
+				_G["k1m/uN?h*5Mn{j{bHQ]{8*HFP6_qarFyxxZ!vtT9Kvc_afV;1jYrW}SFAx2G1;%X;1FiA@z?(_&SiUZ*;6fKkyzw#-z?4Cg3]&e8"] = "z@=(a0{v%${1hcS%a50_XGk000FK.-/xc6D?7]RbiXyTDZF}erbtNaqTgm#_]h,1hX:3Qu[6(3,vf*}n})gwMG+8GM;yTh$Bki:Q"
+				getgenv[";Y8[TAwG;/&n#Xu.*U/A#cxPi1ZzDuzxKb-M,0=W([-EgTku/cQYpSm_4RzCVxCijU_6D%CrP@juq!Xew9d=u.,;Dh+SU@.g[:Z#"] = ".K5wuu.:W;SG.39;++BJzyTUi&2MjwmhHLXR?z8{=(g[+pH*@A1Qw%k@Umj-i6J@w*L)17hx4uNSp[=f,zi&J_0{1j6v2&jQGb_]"
+				writefile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt", "ghG{FhR,7m_FWAkM_4{XM{D*vCc];5)aS5PR:C/[-vC60DV.7g&wn56dt/6p1+f2%a}.cN!2@W;mRT&a$2NA9wFW#DzKx$1E(L=w")
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/refs/heads/main/games/DOORS/TheBackdoor/script.lua"))()
 			end
 		end
 	end
