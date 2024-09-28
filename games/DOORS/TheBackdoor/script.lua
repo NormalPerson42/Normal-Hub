be0cfaa8b0bbd118065f41e9bfbc510adf6f8aaf
NormalPerson42 <shevchenko9480@gmail.com>
2024-09-28 13:51:52 +0200

    Create script.lua

---
+++ games/DOORS/TheBackdoor/script.lua
@@ -0,0 +1,20 @@
+if _G["k1m/uN?h*5Mn{j{bHQ]{8*HFP6_qarFyxxZ!vtT9Kvc_afV;1jYrW}SFAx2G1;%X;1FiA@z?(_&SiUZ*;6fKkyzw#-z?4Cg3]&e8"] == "z@=(a0{v%${1hcS%a50_XGk000FK.-/xc6D?7]RbiXyTDZF}erbtNaqTgm#_]h,1hX:3Qu[6(3,vf*}n})gwMG+8GM;yTh$Bki:Q" then
+	if getgenv[";Y8[TAwG;/&n#Xu.*U/A#cxPi1ZzDuzxKb-M,0=W([-EgTku/cQYpSm_4RzCVxCijU_6D%CrP@juq!Xew9d=u.,;Dh+SU@.g[:Z#"] == ".K5wuu.:W;SG.39;++BJzyTUi&2MjwmhHLXR?z8{=(g[+pH*@A1Qw%k@Umj-i6J@w*L)17hx4uNSp[=f,zi&J_0{1j6v2&jQGb_]" then
+		if isfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt") then
+			if readfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt") == "ghG{FhR,7m_FWAkM_4{XM{D*vCc];5)aS5PR:C/[-vC60DV.7g&wn56dt/6p1+f2%a}.cN!2@W;mRT&a$2NA9wFW#DzKx$1E(L=w" then
+        print("Checks passed")
+				_G["k1m/uN?h*5Mn{j{bHQ]{8*HFP6_qarFyxxZ!vtT9Kvc_afV;1jYrW}SFAx2G1;%X;1FiA@z?(_&SiUZ*;6fKkyzw#-z?4Cg3]&e8"] = nil
+				getgenv[";Y8[TAwG;/&n#Xu.*U/A#cxPi1ZzDuzxKb-M,0=W([-EgTku/cQYpSm_4RzCVxCijU_6D%CrP@juq!Xew9d=u.,;Dh+SU@.g[:Z#"] = nil
+				delfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt")
+			else
+				error("🤣🤣🤣 bro thinks he's smart 🤣🤣🤣")
+			end
+		else
+			error("🤣🤣🤣 bro thinks he's smart 🤣🤣🤣")
+		end
+	else
+		error("🤣🤣🤣 bro thinks he's smart 🤣🤣🤣")
+	end
+else
+	error("🤣🤣🤣 bro thinks he's smart 🤣🤣🤣")
+end
