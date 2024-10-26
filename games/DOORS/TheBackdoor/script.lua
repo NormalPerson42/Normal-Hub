538f4ac14d178b6c4b8412f6fa35651af325d38c
NormalPerson42 <shevchenko9480@gmail.com>
2024-10-26 08:47:57 +0200

    Update script.lua

--- games/DOORS/TheBackdoor/script.lua
+++ games/DOORS/TheBackdoor/script.lua
@@ -2,10 +2,11 @@ if _G["k1m/uN?h*5Mn{j{bHQ]{8*HFP6_qarFyxxZ!vtT9Kvc_afV;1jYrW}SFAx2G1;%X;1FiA@z?(
 	if getgenv()[";Y8[TAwG;/&n#Xu.*U/A#cxPi1ZzDuzxKb-M,0=W([-EgTku/cQYpSm_4RzCVxCijU_6D%CrP@juq!Xew9d=u.,;Dh+SU@.g[:Z#"] == ".K5wuu.:W;SG.39;++BJzyTUi&2MjwmhHLXR?z8{=(g[+pH*@A1Qw%k@Umj-i6J@w*L)17hx4uNSp[=f,zi&J_0{1j6v2&jQGb_]" then
 		if isfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt") then
 			if readfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt") == "ghG{FhR,7m_FWAkM_4{XM{D*vCc];5)aS5PR:C/[-vC60DV.7g&wn56dt/6p1+f2%a}.cN!2@W;mRT&a$2NA9wFW#DzKx$1E(L=w" then
-				print("Checks passed")
 				_G["k1m/uN?h*5Mn{j{bHQ]{8*HFP6_qarFyxxZ!vtT9Kvc_afV;1jYrW}SFAx2G1;%X;1FiA@z?(_&SiUZ*;6fKkyzw#-z?4Cg3]&e8"] = nil
 				getgenv()[";Y8[TAwG;/&n#Xu.*U/A#cxPi1ZzDuzxKb-M,0=W([-EgTku/cQYpSm_4RzCVxCijU_6D%CrP@juq!Xew9d=u.,;Dh+SU@.g[:Z#"] = nil
 				delfile("G2gybc*([;!!09AQZBVTpRc64gW)57S0{xr7i5cA?cCdvQy,RKW)zJWRZAqRkPguxH44V(],HyhkV[BfUB](=3V;ZaXk_/S5r!W8.txt")
+
+				-- start the script i guess
 			else
 				error("Failed to load the script! Please retry again.")
 			end
