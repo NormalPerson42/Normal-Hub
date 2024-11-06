7c26f1bd02e47ccbc5d1370d5f5c59cf5566f1a5
NormalPerson42 <shevchenko9480@gmail.com>
2024-11-06 14:17:33 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -43,7 +43,7 @@ if CheckGame() == nil then
 	print("Game not supported. Launching the universal script...")
 elseif CheckGame() == "Doors_TheBackdoor" then 	
 	print("Found game 'Doors - The Backdoor'. Launching the script...")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/refs/heads/main/games/Realistic%20Street%20Soccer/4v4/script.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/refs/heads/main/games/DOORS/TheBackdoor/script.lua"))()
 	getgenv()["Loader Started"] = false
 elseif CheckGame() == "RealisticStreetSoccer_4v4_nonpro" then 
 	print("Found game 'Realistic Street Soccer - 4v4'. Launching the script...")
