9eef4d358c62ef1d5547f26f3cf6c032520f8218
NormalPerson42 <shevchenko9480@gmail.com>
2025-02-08 17:45:14 +0100

    Update constants.lua

--- constants.lua
+++ constants.lua
@@ -1,1 +1,62 @@
-return "0.1", {}, {5275822877}
+local lib = {}
+
+lib.SupportedGamesID = {}
+lib.BlacklistedUsers = {}
+lib.PremiumUsers = {"EVA12VIRA", "Bog123407t"}
+
+lib.GetPlaceID = function()
+    return game.PlaceId
+end
+
+lib.GetPlaceName = function()
+    return game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+lib.CheckGameSupport = function()
+    for i,v in pairs(lib.SupportedGamesID) do
+        if v == lib.GetPlaceID() then
+            return true
+        end
+    end
+    return false
+end
+
+lib.GetUniversalScript = function()
+    return game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/refs/heads/main/games/Universal.lua")
+end
+
+lib.GetGameScript = function()
+    return game:HttpGet("https://raw.githubusercontent.com/NormalPerson42/Normal-Hub/refs/heads/main/games/"..tostring(game.PlaceId)..".lua")
+end
+
+lib.GetPlayerUserID = function()
+    return game.Players.LocalPlayer.UserId
+end
+
+lib.GetPlayerUserName = function()
+    return game.Players.LocalPlayer.Name
+end
+
+lib.GetPlayerDisplayName = function()
+    return game.Players.LocalPlayer.DisplayName
+end
+
+lib.GetPlayerBlacklist = function()
+    for i,v in pairs(lib.BlacklistedUsers) do
+        if v == lib.GetPlayerUserID() then
+            return true
+        end
+    end
+    return false
+end
+
+lib.IsPremium = function()
+    for i,v in pairs(lib.PremiumUsers) do
+        if v == lib.GetPlayerUserName then
+            return true
+        end
+    end
+    return false
+end
+
+return lib
